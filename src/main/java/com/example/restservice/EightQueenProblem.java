8071a64057c90a5ba54abc3a3783615eefbb2464
unknown <akuzhekov@gmail.com>
2022-06-20 19:18:33 +0600

    fixed serious errors

--- src/main/java/com/example/restservice/EightQueenProblem.java
+++ src/main/java/com/example/restservice/EightQueenProblem.java
@@ -1,9 +1,37 @@
 package com.example.restservice;
+import java.util.HashSet;
 
 public class EightQueenProblem {
 	public int row;
 	public int column;
 	public int N = 8;
+
+	public boolean checkSolution(int board[][]) {
+		HashSet<Integer> row = new HashSet<Integer>();
+		HashSet<Integer> col = new HashSet<Integer>();
+		HashSet<Integer> mainDiag = new HashSet<Integer>();
+		HashSet<Integer> revDiag = new HashSet<Integer>();
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (board[i][j] == 1) {
+					row.add(i);
+					col.add(j);
+					mainDiag.add(i-j);
+					revDiag.add(i+j);
+				}
+			}
+		}
+
+		System.out.println("ROW SIZE: " + row.size());
+		System.out.println("COLUMN SIZE: " + col.size());
+		System.out.println("MAIN DIAGONAL SIZE: " + mainDiag.size());
+		System.out.println("REVERSE DIAGONAL SIZE: " + revDiag.size());
+		return 
+			row.size() == col.size() && 
+			col.size() == mainDiag.size() && 
+			mainDiag.size() == revDiag.size();
+	}
 	
 	public int getChessNum(String symbol) {
 		if (symbol.equals("1") || symbol.equals("a")) {
@@ -33,10 +61,9 @@ public class EightQueenProblem {
 	boolean isSafe(int board[][], int row, int col)
 	{
 		int i, j;
-
 		
-		for (i = 0; i < col; i++)
-			if (board[row][i] == 1)
+		for (i = 0; i < N; i++)
+			if (board[row][i] == 1 || board[i][col] == 1)
 				return false;
 
 		
@@ -44,29 +71,40 @@ public class EightQueenProblem {
 			if (board[i][j] == 1)
 				return false;
 
+		for (i = row+1, j = col+1; i < N && j < N; i++, j++)
+			if (board[i][j] == 1)
+				return false;
 		
-		for (i = row, j = col; j >= 0 && i < N; i++, j--)
+
+		for (i = row, j = col; i < N && j >= 0; i++, j--)
+			if (board[i][j] == 1)
+				return false;
+
+		for (i = row-1, j = col+1; i >= 0 && j < N; i--, j++)
 			if (board[i][j] == 1)
 				return false;
 
 		return true;
 	}
 	
-	boolean solveNEUtil(int board[][], int col)
+	boolean solveNEUtil(int board[][], int r)
 	{
-		
-		if (col >= N)
+
+		if (r == this.row) 
+			r++;
+
+		if (r >= N)
 			return true;
 
 		for (int i = 0; i < N; i++) {
-		
-			if (isSafe(board, i, col)) {
-				board[i][col] = 1;
+			if (isSafe(board, r, i)) {
+				board[r][i] = 1;
 
-				if (solveNEUtil(board, col + 1) == true)
+				if (solveNEUtil(board, r + 1)) {
 					return true;
+				}
 
-				board[i][col] = 0; 
+				board[r][i] = 0; 
 			}
 		}
 
@@ -92,7 +130,7 @@ public class EightQueenProblem {
 			result[i][1] = 0;
 		}
 		
-		if (solveNEUtil(board, 1) == false) {
+		if (solveNEUtil(board, 0) == false) {
 // 			Solution does not exist
 			return result;
 		}
@@ -102,7 +140,7 @@ public class EightQueenProblem {
 		int count = 1;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (board[i][j] == 1 && i != row && j != column) {
+                if (board[i][j] == 1 && (i != row || j != column)) {
                     result[count][0] = i+1;
                     result[count][1] = j+1;
                     count++;
@@ -110,6 +148,17 @@ public class EightQueenProblem {
             }
         }
         
+        System.out.println("--------------------------------------");
+        System.out.println("START POSITION");
+        System.out.println("ROW: " + (this.row+1));
+        System.out.println("COLUMN: " + (this.column+1) + "\n");
+        if (checkSolution(board)) {
+        	System.out.println("\nSolution is right\n");
+        } else {
+        	System.out.println("\nSolution is NOT right. Something is definitely wrong...\n");
+        }
+        System.out.println("--------------------------------------");
+
 		return result;
 	}
 	
